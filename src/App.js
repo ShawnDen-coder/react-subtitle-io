851d02664a6f68000715c2105921f716b0f3f696
denghui <88145482@qq.com>
2022-08-18 18:52:56 +0800

    项目初始化:增加了导航栏模版

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.less'
+import "./App.less";
+import { Col, Menu, Row } from "antd";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 
-function App() {
-    return (
-        <div className="App">
-            <div></div>
-        </div>
-    );
-}
+const App = () => (
+  <>
+    <Row>
+      <Col span={24}>
+        <Menu mode="horizontal" defaultSelectedKeys={["mail"]}>
+          <Menu.Item key="mail" icon={<MailOutlined />}>
+            Navigation One
+          </Menu.Item>
+          <Menu.SubMenu
+            key="SubMenu"
+            title="Navigation Two - Submenu"
+            icon={<SettingOutlined />}
+          >
+            <Menu.Item key="two" icon={<AppstoreOutlined />}>
+              Navigation Two
+            </Menu.Item>
+            <Menu.Item key="three" icon={<AppstoreOutlined />}>
+              Navigation Three
+            </Menu.Item>
+          </Menu.SubMenu>
+        </Menu>
+      </Col>
+    </Row>
+  </>
+);
 
 export default App;
