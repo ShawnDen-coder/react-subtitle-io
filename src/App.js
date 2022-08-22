c5b49743500995a554a940a93e1043132b330201
denghui <88145482@qq.com>
2022-08-22 10:23:33 +0800

    项目初始化:增加了导航栏模版

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import "./App.less";
-import { Col, Menu, Row } from "antd";
-import {
-  AppstoreOutlined,
-  MailOutlined,
-  SettingOutlined,
-  HomeOutlined,
-} from "@ant-design/icons";
+
+import { Nav } from "./layOut";
+import { SubListScreen } from "./subList/subListScreen";
+import { Col, Row } from "antd";
 
 const App = () => (
   <>
+    <Nav />
     <Row>
-      <Col span={24}>
-        <Menu mode="horizontal">
-          <Menu.Item key={"home"} icon={<HomeOutlined />}>
-            Home
-          </Menu.Item>
-
-          <Menu.Item key="mail" icon={<MailOutlined />}>
-            Navigation One
-          </Menu.Item>
-          <Menu.SubMenu
-            key="SubMenu"
-            title="Navigation Two - Submenu"
-            icon={<SettingOutlined />}
-          >
-            <Menu.Item key="two" icon={<AppstoreOutlined />}>
-              Navigation Two
-            </Menu.Item>
-            <Menu.Item key="three" icon={<AppstoreOutlined />}>
-              Navigation Three
-            </Menu.Item>
-          </Menu.SubMenu>
-        </Menu>
+      <Col span={14} offset={5}>
+        <SubListScreen />
       </Col>
     </Row>
   </>
--- src/layOut/index.jsx
+++ src/layOut/index.jsx
@@ -1,35 +1,42 @@
 import React from "react";
 import { Menu } from "antd";
 import {
+  HomeOutlined,
   AppstoreOutlined,
   MailOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
+import styled from "styled-components";
 
-export const Nav = () => (
-  <Menu mode="horizontal" defaultSelectedKeys={["mail"]}>
-    <Menu.Item key="mail" icon={<MailOutlined />}>
-      Navigation One
-    </Menu.Item>
-    <Menu.SubMenu
-      key="SubMenu"
-      title="Navigation Two - Submenu"
-      icon={<SettingOutlined />}
-    >
-      <Menu.Item key="two" icon={<AppstoreOutlined />}>
-        Navigation Two
-      </Menu.Item>
-      <Menu.Item key="three" icon={<AppstoreOutlined />}>
-        Navigation Three
-      </Menu.Item>
-      <Menu.ItemGroup title="Item Group">
-        <Menu.Item key="four" icon={<AppstoreOutlined />}>
-          Navigation Four
+export const Nav = () => {
+  return (
+    <StyledNav>
+      <Menu mode="horizontal">
+        <Menu.Item key="home" icon={<HomeOutlined />}>
+          Home
         </Menu.Item>
-        <Menu.Item key="five" icon={<AppstoreOutlined />}>
-          Navigation Five
+        <Menu.Item key="mail" icon={<MailOutlined />}>
+          Navigation One
         </Menu.Item>
-      </Menu.ItemGroup>
-    </Menu.SubMenu>
-  </Menu>
-);
+        <Menu.SubMenu
+          key="SubMenu"
+          title="Navigation Two - Submenu"
+          icon={<SettingOutlined />}
+        >
+          <Menu.Item key="two" icon={<AppstoreOutlined />}>
+            Navigation Two
+          </Menu.Item>
+
+          <Menu.Item key="three" icon={<AppstoreOutlined />}>
+            Navigation Three
+          </Menu.Item>
+        </Menu.SubMenu>
+      </Menu>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.div`
+  font-size: 50px;
+  color: blue;
+`;
